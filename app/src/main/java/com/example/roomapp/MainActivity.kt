b266ff5821ab76585eb14f53b696e974afb3c2b5
thyspeter <52890767+thyspeter@users.noreply.github.com>
2021-10-07 23:13:19 +0200

    Add en view toegevoegd

---
+++ app/src/main/java/com/example/roomapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.roomapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //zorgt ervoor dat in de controlbar de naam van het fragment komt en niet van de app
+        setupActionBarWithNavController(findNavController(R.id.fragment))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragment)
+        return navController.navigateUp()||super.onSupportNavigateUp()
+    }
+}
